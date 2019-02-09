4b16e6e2bb77914b55d34f6ccd3a661d1fcb2726
Seong Yong-ju <sei40kr@gmail.com>
2019-02-10 03:09:03 +0900

    Listing all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def show
-      @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def new; end
