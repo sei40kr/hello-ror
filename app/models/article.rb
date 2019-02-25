68d39fd960b84ba3934a8c803b32b47b43825dd4
Seong Yong-ju <sei40kr@gmail.com>
2019-02-25 21:22:21 +0900

    Delete an association for a deleted comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destory
   validates :title, presence: true, length: { minimum: 5 }
 end
